dc6d252e5d2f18867716314ed7cfea2328966bbe
Sabina Trufin <sabina.iuliana.trufin@gmail.com>
2025-01-14 22:11:50 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace OpenTK3_StandardTemplate_WinForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showAxes = new System.Windows.Forms.CheckBox();
+            this.changeBackground = new System.Windows.Forms.Button();
+            this.lblOx = new System.Windows.Forms.Label();
+            this.lblOy = new System.Windows.Forms.Label();
+            this.lblOz = new System.Windows.Forms.Label();
+            this.resetScene = new System.Windows.Forms.Button();
+            this.enableRotation = new System.Windows.Forms.CheckBox();
+            this.enableObjectRotation = new System.Windows.Forms.CheckBox();
+            this.Transparency = new System.Windows.Forms.Button();
+            this.Texture1 = new System.Windows.Forms.RadioButton();
+            this.texture2 = new System.Windows.Forms.RadioButton();
+            this.light = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // showAxes
+            // 
+            this.showAxes.AutoSize = true;
+            this.showAxes.Checked = true;
+            this.showAxes.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.showAxes.Location = new System.Drawing.Point(2232, 31);
+            this.showAxes.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.showAxes.Name = "showAxes";
+            this.showAxes.Size = new System.Drawing.Size(193, 36);
+            this.showAxes.TabIndex = 0;
+            this.showAxes.Text = "Show Axes";
+            this.showAxes.UseVisualStyleBackColor = true;
+            this.showAxes.CheckedChanged += new System.EventHandler(this.showAxes_CheckedChanged);
+            // 
+            // changeBackground
+            // 
+            this.changeBackground.Location = new System.Drawing.Point(2232, 258);
+            this.changeBackground.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.changeBackground.Name = "changeBackground";
+            this.changeBackground.Size = new System.Drawing.Size(533, 76);
+            this.changeBackground.TabIndex = 1;
+            this.changeBackground.Text = "Change background color";
+            this.changeBackground.UseVisualStyleBackColor = true;
+            this.changeBackground.Click += new System.EventHandler(this.changeBackground_Click);
+            // 
+            // lblOx
+            // 
+            this.lblOx.BackColor = System.Drawing.Color.Red;
+            this.lblOx.Location = new System.Drawing.Point(2291, 83);
+            this.lblOx.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblOx.Name = "lblOx";
+            this.lblOx.Size = new System.Drawing.Size(107, 48);
+            this.lblOx.TabIndex = 2;
+            this.lblOx.Text = "Ox";
+            this.lblOx.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOy
+            // 
+            this.lblOy.BackColor = System.Drawing.Color.Green;
+            this.lblOy.Location = new System.Drawing.Point(2413, 83);
+            this.lblOy.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblOy.Name = "lblOy";
+            this.lblOy.Size = new System.Drawing.Size(107, 48);
+            this.lblOy.TabIndex = 3;
+            this.lblOy.Text = "Oy";
+            this.lblOy.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblOz
+            // 
+            this.lblOz.BackColor = System.Drawing.Color.Blue;
+            this.lblOz.Location = new System.Drawing.Point(2536, 83);
+            this.lblOz.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.lblOz.Name = "lblOz";
+            this.lblOz.Size = new System.Drawing.Size(107, 48);
+            this.lblOz.TabIndex = 4;
+            this.lblOz.Text = "Oz";
+            this.lblOz.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resetScene
+            // 
+            this.resetScene.Location = new System.Drawing.Point(2232, 348);
+            this.resetScene.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.resetScene.Name = "resetScene";
+            this.resetScene.Size = new System.Drawing.Size(533, 76);
+            this.resetScene.TabIndex = 5;
+            this.resetScene.Text = "Reset scene";
+            this.resetScene.UseVisualStyleBackColor = true;
+            this.resetScene.Click += new System.EventHandler(this.resetScene_Click);
+            // 
+            // enableRotation
+            // 
+            this.enableRotation.AutoSize = true;
+            this.enableRotation.Location = new System.Drawing.Point(2232, 138);
+            this.enableRotation.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.enableRotation.Name = "enableRotation";
+            this.enableRotation.Size = new System.Drawing.Size(337, 36);
+            this.enableRotation.TabIndex = 6;
+            this.enableRotation.Text = "Enable mouse rotation";
+            this.enableRotation.UseVisualStyleBackColor = true;
+            // 
+            // enableObjectRotation
+            // 
+            this.enableObjectRotation.AutoSize = true;
+            this.enableObjectRotation.Location = new System.Drawing.Point(2232, 198);
+            this.enableObjectRotation.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.enableObjectRotation.Name = "enableObjectRotation";
+            this.enableObjectRotation.Size = new System.Drawing.Size(329, 36);
+            this.enableObjectRotation.TabIndex = 7;
+            this.enableObjectRotation.Text = "Enable object rotation";
+            this.enableObjectRotation.UseVisualStyleBackColor = true;
+            // 
+            // Transparency
+            // 
+            this.Transparency.Location = new System.Drawing.Point(2232, 434);
+            this.Transparency.Name = "Transparency";
+            this.Transparency.Size = new System.Drawing.Size(533, 71);
+            this.Transparency.TabIndex = 8;
+            this.Transparency.Text = "alpha-blending";
+            this.Transparency.UseVisualStyleBackColor = true;
+            this.Transparency.Click += new System.EventHandler(this.Transparency_Click);
+            // 
+            // Texture1
+            // 
+            this.Texture1.AutoSize = true;
+            this.Texture1.Location = new System.Drawing.Point(2232, 600);
+            this.Texture1.Name = "Texture1";
+            this.Texture1.Size = new System.Drawing.Size(154, 36);
+            this.Texture1.TabIndex = 9;
+            this.Texture1.TabStop = true;
+            this.Texture1.Text = "texture1";
+            this.Texture1.UseVisualStyleBackColor = true;
+            this.Texture1.CheckedChanged += new System.EventHandler(this.Texture1_CheckedChanged);
+            // 
+            // texture2
+            // 
+            this.texture2.AutoSize = true;
+            this.texture2.Location = new System.Drawing.Point(2232, 669);
+            this.texture2.Name = "texture2";
+            this.texture2.Size = new System.Drawing.Size(154, 36);
+            this.texture2.TabIndex = 10;
+            this.texture2.TabStop = true;
+            this.texture2.Text = "texture2";
+            this.texture2.UseVisualStyleBackColor = true;
+            this.texture2.CheckedChanged += new System.EventHandler(this.texture2_CheckedChanged);
+            // 
+            // light
+            // 
+            this.light.Location = new System.Drawing.Point(2232, 519);
+            this.light.Name = "light";
+            this.light.Size = new System.Drawing.Size(533, 75);
+            this.light.TabIndex = 11;
+            this.light.Text = "Iluminare";
+            this.light.UseVisualStyleBackColor = true;
+            this.light.Click += new System.EventHandler(this.light_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2797, 1488);
+            this.Controls.Add(this.light);
+            this.Controls.Add(this.texture2);
+            this.Controls.Add(this.Texture1);
+            this.Controls.Add(this.Transparency);
+            this.Controls.Add(this.enableObjectRotation);
+            this.Controls.Add(this.enableRotation);
+            this.Controls.Add(this.resetScene);
+            this.Controls.Add(this.lblOz);
+            this.Controls.Add(this.lblOy);
+            this.Controls.Add(this.lblOx);
+            this.Controls.Add(this.changeBackground);
+            this.Controls.Add(this.showAxes);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "MainForm";
+            this.Text = "OpenTK 3";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox showAxes;
+        private System.Windows.Forms.Button changeBackground;
+        private System.Windows.Forms.Label lblOx;
+        private System.Windows.Forms.Label lblOy;
+        private System.Windows.Forms.Label lblOz;
+        private System.Windows.Forms.Button resetScene;
+        private System.Windows.Forms.CheckBox enableRotation;
+        private System.Windows.Forms.CheckBox enableObjectRotation;
+        private System.Windows.Forms.Button Transparency;
+        private System.Windows.Forms.RadioButton Texture1;
+        private System.Windows.Forms.RadioButton texture2;
+        private System.Windows.Forms.Button light;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+using OpenTK.Graphics.OpenGL;
+
+using OpenTK3_StandardTemplate_WinForms.helpers;
+using OpenTK3_StandardTemplate_WinForms.objects;
+
+namespace OpenTK3_StandardTemplate_WinForms
+{
+    public partial class MainForm : Form
+    {
+        private Axes mainAxis;
+        private Rectangles re;
+        private Camera cam;
+        private Scene scene;
+        private Cube cub;
+
+
+        private bool isTransparent = false;
+        private bool? t1 = null;
+        private int[] textures = new int[2];
+        private Point mousePosition;
+
+        private float[] valuesAmbient0 = new float[4];
+        private float[] valuesDiffuse0 = new float[4];
+        private float[] valuesSpecular0 = new float[4];
+        private float[] valuesPosition0 = new float[4];
+
+        private bool lightON = false;
+        private bool lightON_0 = false; 
+        
+
+
+        public MainForm()
+        {   
+            // general init
+            InitializeComponent();
+
+            // init VIEWPORT
+            scene = new Scene();
+
+            scene.GetViewport().Load += new EventHandler(this.mainViewport_Load);
+            scene.GetViewport().Paint += new PaintEventHandler(this.mainViewport_Paint);
+            scene.GetViewport().MouseMove += new MouseEventHandler(this.mainViewport_MouseMove);
+
+            this.Controls.Add(scene.GetViewport());
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // init RNG
+            Randomizer.Init();
+
+            // init CAMERA/EYE
+            cam = new Camera(scene.GetViewport());
+
+            // init AXES
+            mainAxis = new Axes(showAxes.Checked);
+            // re = new Rectangles();
+            cub = new Cube();
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Less);
+            LoadTextures();
+        }
+
+        private void InitializeLighting()
+        {
+            float[] valuesAmbientTemplate0 = new float[] { 0.1f, 0.1f, 0.1f, 1.0f };
+            float[] valuesDiffuseTemplate0 = new float[] { 1.0f, 1.0f, 1.0f, 1.0f };
+            float[] valuesSpecularTemplate0 = new float[] { 0.1f, 0.1f, 0.1f, 1.0f };
+            float[] valuesPositionTemplate0 = new float[] { 0.0f, 0.0f, 5.0f, 1.0f };
+
+            for (int i = 0; i < valuesAmbientTemplate0.Length; i++)
+            {
+                valuesAmbient0[i] = valuesAmbientTemplate0[i];
+                valuesSpecular0[i] = valuesSpecularTemplate0[i];
+                valuesPosition0[i] = valuesPositionTemplate0[i];
+                valuesDiffuse0[i] = valuesDiffuseTemplate0[i];
+            }
+        }
+        private void showAxes_CheckedChanged(object sender, EventArgs e)
+        {
+            mainAxis.SetVisibility(showAxes.Checked);
+
+            scene.Invalidate();
+        }
+
+        private void changeBackground_Click(object sender, EventArgs e)
+        {
+            GL.ClearColor(Randomizer.GetRandomColor());
+
+            scene.Invalidate();
+        }
+
+        private void resetScene_Click(object sender, EventArgs e)
+        {
+            showAxes.Checked = true;
+            mainAxis.SetVisibility(showAxes.Checked);
+            scene.Reset();
+            cam.Reset();
+            isTransparent = false;
+           Transparency.Text = "Transparency: OFF";
+            
+
+            enableRotation.Checked = false;
+            enableObjectRotation.Checked = false;
+
+            t1 = null;
+            cub.SetTexture(0);
+            Texture1.Checked = false;
+            texture2.Checked = false;
+
+            InitializeLighting();
+            lightON = false;
+            lightON_0 = false;
+            light.Text = "Iluminare OFF";
+            
+
+            scene.Invalidate();
+        }
+
+        private void mainViewport_Load(object sender, EventArgs e)
+        {
+            scene.Reset();
+        }
+
+        private void mainViewport_MouseMove(object sender, MouseEventArgs e)
+        {
+            mousePosition = new Point(e.X, e.Y);
+            scene.Invalidate();
+        }
+
+        private void mainViewport_Paint(object sender, PaintEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.Clear(ClearBufferMask.DepthBufferBit);
+
+            cam.SetView();
+
+            if (enableRotation.Checked == true)
+            {
+                // Doar după axa Ox.
+                GL.Rotate(Math.Max(mousePosition.X, mousePosition.Y), 1, 1, 1);
+            }
+            GL.Enable(EnableCap.Texture2D);
+            GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+
+            // GRAPHICS PAYLOAD
+            mainAxis.Draw();
+
+            if (lightON)
+            {
+                GL.Enable(EnableCap.Lighting);
+                
+                GL.Light(LightName.Light0, LightParameter.Diffuse, valuesDiffuse0);
+                GL.Light(LightName.Light0, LightParameter.Specular, valuesSpecular0);
+
+                if (lightON_0)
+                {
+                    GL.Enable(EnableCap.Light0);
+                }
+                else
+                {
+                    GL.Disable(EnableCap.Light0);
+                }
+            }
+            else
+            {
+                GL.Disable(EnableCap.Lighting);
+            }
+
+            if (enableObjectRotation.Checked)
+            {
+                GL.PushMatrix();
+                GL.Rotate(Math.Max(mousePosition.X, mousePosition.Y), 1, 1, 1);
+
+                cub.DrawCube(isTransparent ? 0.5f : 1.0f);
+
+                GL.PopMatrix();
+            }
+            else
+            {
+                cub.DrawCube(isTransparent ? 0.5f : 1.0f);
+            }
+
+            scene.GetViewport().SwapBuffers();
+        }
+
+        private void LoadTextures()
+        {
+            GL.GenTextures(textures.Length, textures);
+            LoadTexture(textures[0], "t1.jpg");
+            LoadTexture(textures[1], "t2.jpg");
+        }
+
+        private void LoadTexture(int textureId, string filename)
+        {
+            Bitmap bmp = new Bitmap(filename);
+
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
+                                                    System.Drawing.Imaging.ImageLockMode.ReadOnly,
+                                                    System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.BindTexture(TextureTarget.Texture2D, textureId);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba,
+                          bmp.Width, bmp.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra,
+                          PixelType.UnsignedByte, data.Scan0);
+
+            bmp.UnlockBits(data);
+
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (float)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (float)TextureMagFilter.Linear);
+        }
+
+       
+
+        private void Transparency_Click(object sender, EventArgs e)
+        {
+
+            isTransparent = !isTransparent;
+            Transparency.Text = isTransparent ? "Transparency: ON" : "Transparency: OFF";
+
+            scene.Invalidate();
+        }
+
+        private void Texture1_CheckedChanged(object sender, EventArgs e)
+        {
+            t1 = true;
+            cub.SetTexture(textures[0]);
+            scene.Invalidate();
+        }
+
+        private void texture2_CheckedChanged(object sender, EventArgs e)
+        {
+            t1 = false;
+            cub.SetTexture(textures[1]);
+            scene.Invalidate();
+        }
+
+        private void light_Click(object sender, EventArgs e)
+        {
+            lightON = !lightON;
+            light.Text = lightON ? "Iluminare ON" : "Iluminare OFF";
+            scene.Invalidate();
+        }
+    }
+}
